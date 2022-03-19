3ad6ed8561f7e9c3e977b454a68a25e9d7e075f1
elfalehdev <mohamedelfaleh5@gmail.com>
2022-03-19 16:12:16 +0100

    :zap: Adding webview and custom commands

--- out/extension.js
+++ out/extension.js
@@ -1,28 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-var counter = 0;
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const HelloWorldPanel_1 = require("./HelloWorldPanel");
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscare" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('vscare', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello this is vscare');
+    context.subscriptions.push(vscode.commands.registerCommand("vscare.Test", () => {
+        HelloWorldPanel_1.HelloWorldPanel.createOrShow(context.extensionUri);
+    }));
+    vscode.commands.registerCommand("vscare", async () => {
+        const answer = await vscode.window.showInformationMessage("How was your day?", "good", "bad");
+        if (answer === "bad") {
+            vscode.window.showInformationMessage("Sorry to hear that");
+        }
+        else {
+            vscode.window.showInformationMessage("Glad to hear that.");
+        }
+        function update() {
+            vscode.window.showInformationMessage("Straighten your position");
+        }
+        setTimeout(update, 1000);
     });
-    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-// this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
-
-var counter = 0;
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import { HelloWorldPanel } from './HelloWorldPanel';
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscare" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscare', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vscare.Test",() => {
+        HelloWorldPanel.createOrShow(context.extensionUri);
+    })
+  );
 
-		vscode.window.showInformationMessage('Hello this is vscare');
+    vscode.commands.registerCommand("vscare", async () => {
+      const answer = await vscode.window.showInformationMessage(
+        "How was your day?",
+        "good",
+        "bad"
+      );
+
+      if (answer === "bad") {
+        vscode.window.showInformationMessage("Sorry to hear that");
+      } else {
+        vscode.window.showInformationMessage("Glad to hear that.");
+      }
+
+	  function update(){
+		  vscode.window.showInformationMessage("Straighten your position");
+	  }
+	  setTimeout(update,1000);
 	});
-
-	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void {}
